9642b35fb3b1218c72811d67fcb36084f48ebb14
Jordancsb <jcaindoor@gmail.com>
2022-07-18 22:44:39 -0300

    feat: make models

---
+++ backend/app/Models/Company.ts
@@ -0,0 +1,43 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Company extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public razao: string
+
+  @column()
+  public fantasia: string
+
+  @column()
+  public cnpj: string
+
+  @column()
+  public rua: string
+
+  @column()
+  public numero: string
+
+  @column()
+  public complemento: string
+
+  @column()
+  public bairro: string
+
+  @column()
+  public cidade: string
+
+  @column()
+  public estado: string
+
+  @column()
+  public telefone: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
